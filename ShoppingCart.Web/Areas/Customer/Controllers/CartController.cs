c92a7724f7c731c6e61b56510dd6741ae5deaf0e
miraytheone <84405003+miraytheone@users.noreply.github.com>
2022-12-20 19:01:46 +0300

    Add files via upload

---
+++ ShoppingCart.Web/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,103 @@
+﻿
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.DataAccess.Repositories;
+using ShoppingCart.DataAccess.ViewModels;
+using ShoppingCart.Models;
+using System.Security.Claims;
+
+namespace ShoppingCart.Web.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize(Roles ="Admin")]
+    public class CartController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public CartVM vm { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            vm = new CartVM()
+            {
+                ListOfCart = _unitOfWork.Cart.GetAll(x => x.ApplicationUserId == claims.Value, includeProperties: "Product"),
+                  OrderHeader = new OrderHeader()
+            };
+
+            foreach(var item in vm.ListOfCart)
+            {
+                vm.OrderHeader.OrderTotal += (item.Product.Price + item.Count);
+            }
+
+            return View(vm);
+        }
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            vm = new CartVM()
+            {
+                ListOfCart = _unitOfWork.Cart.GetAll(x => x.ApplicationUserId == claims.Value, includeProperties: "Product"),
+                OrderHeader = new OrderHeader()
+            };
+            vm.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.GetT(x => x.Id == claims.Value);
+            vm.OrderHeader.Name = vm.OrderHeader.ApplicationUser.Name;
+            vm.OrderHeader.PhoneNumber = vm.OrderHeader.ApplicationUser.PhoneNumber;
+            vm.OrderHeader.Address = vm.OrderHeader.ApplicationUser.Address;
+            vm.OrderHeader.City = vm.OrderHeader.ApplicationUser.City;
+            vm.OrderHeader.State = vm.OrderHeader.ApplicationUser.State;
+            vm.OrderHeader.PinCode = vm.OrderHeader.ApplicationUser.PinCode;
+
+            foreach (var item in vm.ListOfCart)
+            {
+                vm.OrderHeader.OrderTotal += (item.Product.Price + item.Count);
+            }
+
+            return View(vm);
+        }
+
+        [HttpGet]
+        public IActionResult plus(int id)
+        {
+            var cart = _unitOfWork.Cart.GetT(x => x.Id == id);
+            _unitOfWork.Cart.IncrementCartItem(cart, 1);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult minus(int id)
+        {
+            var cart = _unitOfWork.Cart.GetT(x => x.Id == id);
+            if(cart.Count <= 1)
+            {
+                _unitOfWork.Cart.Delete(cart);
+            }
+            else
+            {
+                _unitOfWork.Cart.DecrementCartItem(cart, 1);
+            }
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult delete(int id)
+        {
+            var cart = _unitOfWork.Cart.GetT(x => x.Id == id);
+            _unitOfWork.Cart.Delete(cart);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
